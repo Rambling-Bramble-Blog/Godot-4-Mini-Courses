3eac544e9910e9fde91d726997a2d6d022679fc3
Rambling-Bramble-Blog <rambling.bramble.blog@gmail.com>
2026-01-15 15:24:58 -0800

    A: First Mini Game Balloons Started. Parts 1 to 3.

---
+++ Balloon Popper/balloon.gd
@@ -0,0 +1,8 @@
+extends Area3D
+
+@export var clicks_to_pop : int = 5
+@export var size_increase : float = 0.2
+@export var score_to_give : int = 1
+
+func _on_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	pass
