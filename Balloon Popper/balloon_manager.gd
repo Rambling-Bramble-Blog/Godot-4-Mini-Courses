51e1c830b2db6325fb5162f6bcab83bdd236bbc8
Rambling-Bramble-Blog <rambling.bramble.blog@gmail.com>
2026-01-15 20:14:30 -0800

    D: Score Label added. Part 6. Finished.

--- Balloon Popper/balloon_manager.gd
+++ Balloon Popper/balloon_manager.gd
@@ -1,7 +1,13 @@
 extends Node3D
 
 var score :int = 0
+var score_text : Label
+
+func _ready() -> void:
+	score_text = $ScoreText
+	score_text.text = "Score: 0"
 
 func increase_score(amount :int):
 	score += amount
+	score_text.text = "Score: " + str(score)
 	print(score)
